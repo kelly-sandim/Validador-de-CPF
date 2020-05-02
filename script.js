2ca7cab8d128c180ee176729d9fedb835e741818
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-05-02 15:32:39 -0400

    Segunda parte do validador de CPF feita

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-console.log("Javascript carregado");
+console.log('Javascript carregado');
+
+function validaCPF(cpf) {
+    return false;
+}
 
 function validacao() {
-    console.log("Iniciando validação CPF");
+    console.log('Iniciando validação CPF');
+    var cpf = document.getElementById('cpf_digitado').value;
+    
+    var resultadoValidacao = validaCPF(cpf);
+
+    if(resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
 }
