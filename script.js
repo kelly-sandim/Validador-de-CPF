984637df00a40dd009343df8305531a0a399ccfe
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-05-02 15:45:31 -0400

    Terceira parte do validador de CPF feita

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 console.log('Javascript carregado');
 
 function validaCPF(cpf) {
-    return false;
+    if(cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0, 9);  //A partir do 0, eu quero pegar 9 caracteres
+        var digitos = cpf.substring(9);
+
+        var soma = 0;
+        for(var i = 10; i > 1; i --) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        console.log(soma);
+        
+        return true;
+    }
 }
 
 function validacao() {
